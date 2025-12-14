68a05dafd96785aae71a4aa08471b9ec18ae1b69
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2025-12-14 12:06:06 +0900

    feat: adiciona exemplo basico de variaveis

---
+++ basico/variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 0;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Idade informada: %d\n", idade);
+
+    return 0;
+}
