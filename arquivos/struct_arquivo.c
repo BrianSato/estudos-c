436f54a42ca5f2e0332cfb8aeede19648b716d73
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2025-12-22 03:14:43 +0900

    feat: salva dados de struct em arquivo texto

---
+++ arquivos/struct_arquivo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Definicao da struct
+struct Aluno {
+    char nome[50];
+    int idade;
+};
+
+int main() {
+    struct Aluno aluno;
+    FILE *arquivo;
+
+    printf("Digite o nome do aluno: ");
+    scanf("%49s", aluno.nome);
+
+    printf("Digite a idade do aluno: ");
+    scanf("%d", &aluno.idade);
+
+    arquivo = fopen("alunos.txt", "a");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    fprintf(arquivo, "%s %d\n", aluno.nome, aluno.idade);
+    fclose(arquivo);
+
+    printf("Aluno salvo no arquivo com sucesso.\n");
+
+    return 0;
+}
+
+    
+        
