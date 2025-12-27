75a05ebc6dab3ff3e998dd3f3418ea180a2ee801
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2025-12-27 17:12:21 +0900

    feat: implementa listagem de alunos no menu

--- projeto/cadastro_alunos.c
+++ projeto/cadastro_alunos.c
@@ -35,6 +35,26 @@ void cadastrarAluno() {
     printf("Aluno cadastrado com sucesso!\n");
 }
 
+void listarAlunos() {
+    struct Aluno aluno;
+    FILE *arquivo;
+
+    arquivo = fopen("alunos.txt", "r");
+    if (arquivo == NULL) {
+        printf("Nenhum aluno cadastrado ainda.\n");
+        return;
+    }
+
+    printf("\nLista de alunos cadastrados:\n");
+
+    while (fscanf(arquivo, "%49s %d", aluno.nome, &aluno.idade) != EOF) {
+        printf("Nome: %s | Idade: %d\n", aluno.nome, aluno.idade);
+    }
+
+    fclose(arquivo);
+}
+
+
 int main() {
     int opcao;
 
@@ -47,7 +67,7 @@ int main() {
                 cadastrarAluno();
                 break;
             case 2:
-                printf("Listar alunos (em breve)\n");
+               listarAlunos();
                 break;
             case 0:
                 printf("Saindo do programa...\n");
