29c754e2b751d15756e04fb8b4ace742b0f82645
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2025-12-30 17:01:06 +0900

    Create cadastro_alunos.h

---
+++ projeto/cadastro_alunos.h
@@ -0,0 +1,17 @@
+#ifndef CADASTRO_ALUNOS
+#define CADASTRO_ALUNOS
+#include <stdio.h>
+
+//ESTRUTUTA CRIADA PARA UTILIZAR EM CADASTRO_ALUNOS.C
+struct Aluno {
+    char nome[50];
+    int idade;
+};
+
+//PROTÓTIPO DAS FUNÇÕES SALVAS EM FUNCOES_CADASTRO.C
+void menu();
+void cadastrarAluno();
+void salvarAluno(struct Aluno aluno);
+void listarAlunos();
+
+#endif
