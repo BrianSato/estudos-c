fe9f1c8d68977b658e59d9f0757ce3f7d8808821
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-01 15:19:27 +0900

    feat: limpando main mantendo apenas chamada de  menu.c

--- projeto/main_cadastro_aluno.c
+++ projeto/main_cadastro_aluno.c
@@ -1,28 +1,8 @@
 #include "cadastro_alunos.h"
 
 int main() {
-    int opcao;
-
-    do {
-        menu();
-        scanf("%d",&opcao);
-        getchar();
-
-        switch (opcao) {
-            case 1:
-                cadastrarAluno();
-                break;
-            case 2:
-                listarAlunos();
-                break;
-            case 0:
-                printf("Saindo do programa...\n");
-                break;
-            default:
-                printf("Opcao invalida.\n");
-        }
-
-    } while (opcao != 0);
+    
+    menu();
 
     return 0;
 }
