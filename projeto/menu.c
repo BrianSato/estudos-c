1e36da807b8bf2e5e514b714118d99d279063192
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-01 15:18:16 +0900

    feat: Criando menu.c para deixar o main limpo.

---
+++ projeto/menu.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "cadastro_alunos.h"
+
+//MENU PRINCIPAL MOSTRADO NO CONSOLE.
+void menu() {
+	int opcao;
+
+	do{
+	printf("===== MENU =====\n");
+    printf("1 - Cadastrar aluno\n");
+    printf("2 - Listar alunos\n");
+    printf("0 - Sair\n");
+    printf("Escolha uma opcao: ");
+    fflush(stdout);
+    scanf("%d", &opcao);
+    getchar();
+//SWITCH DE OPÇÕES UTILIZANDO AS FUNÇÕES DE FUNCOES_CADASTRO.C
+    switch(opcao){
+      case 1:
+          cadastrarAluno();
+          break;
+     case 2:
+          listarAlunos();
+          break;
+     case 0:
+          printf("Saindo do programa...\n");
+          break;
+     default:
+          printf("Opcao invalida.\n");
+      }
+   }while (opcao != 0);
+}
