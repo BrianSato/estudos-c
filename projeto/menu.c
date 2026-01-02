74d93c56f62a9ccc899bcfec9041b665ef21da55
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-02 13:38:51 +0900

    feat: Adicionando funcao EDITAR ALUNO

--- projeto/menu.c
+++ projeto/menu.c
@@ -9,7 +9,8 @@ void menu() {
 	printf("===== MENU =====\n");
     printf("1 - Cadastrar aluno\n");
     printf("2 - Listar alunos\n");
-	printf("3- Remover aluno:\n");
+    printf("3 - Editar Aluno\n");
+    printf("4 - Apagar Aluno\n");
     printf("0 - Sair\n");
     printf("Escolha uma opcao: ");
     fflush(stdout);
@@ -20,16 +21,18 @@ void menu() {
       case 1:
           cadastrarAluno();
           break;
-      case 2:
+     case 2:
           listarAlunos();
           break;
-	  case 3:
-		  removerAluno();
-		  break;	
-      case 0:
+     case 3:
+    	  editarAluno();
+    	  break;
+     case 4:
+    	 removerAluno();
+     case 0:
           printf("Saindo do programa...\n");
           break;
-      default:
+     default:
           printf("Opcao invalida.\n");
       }
    }while (opcao != 0);
