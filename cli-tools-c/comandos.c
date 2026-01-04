efb17494649e13b18484f84252efd34f9de8efec
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-04 12:45:31 +0900

    feat: Colocando funções -w e -c

--- cli-tools-c/comandos.c
+++ cli-tools-c/comandos.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include"comandos.h"
 
-//LISTA DE FUNÇÕES UTILIZADA
+//LISTA DE FUNÇÕES UTILIZADAS
 int contaLinhas(const char *nomeArquivo){
 	FILE *arquivo;
 	int linhas = 0;
@@ -9,12 +9,47 @@ int contaLinhas(const char *nomeArquivo){
 
 	arquivo = fopen(nomeArquivo, "r");
 	if(arquivo == NULL){
-		printf("Arquito não encontrado");
+		printf("Arquivo não encontrado");
 		return 1;
 	}
 	while(fgets(buffer,sizeof(buffer),arquivo) != NULL){
 	    linhas++;
 	  }
-	return linhas;
 	fclose(arquivo);
+	return linhas;
+}
+
+int contaPalavras(const char *nomeArquivo){
+	FILE *arquivo;
+	int palavras = 0;
+	char buffer[256];
+
+	arquivo = fopen(nomeArquivo, "r");
+	if(arquivo == NULL){
+		printf("Arquivo não encontrado");
+		return 1;
+	}
+	while(fscanf(arquivo,"%s",buffer) == 1){
+		palavras ++;
+	}
+	fclose(arquivo);
+	return palavras;
+}
+
+int contaCaracteres(const char *nomeArquivo){
+	FILE *arquivo;
+	int caracteres = 0;
+	int c;
+
+	arquivo = fopen(nomeArquivo, "r");
+	if(arquivo == NULL){
+		printf("Arquivo não encontrado");
+		return 1;
+	}
+	while((c = fgetc(arquivo)) != EOF){
+		caracteres ++;
+	}
+	fclose(arquivo);
+	return caracteres;
+
 }
