e75a97d5c5f0d029d0541735c2ed09a017023200
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-04 12:44:34 +0900

    feat: Colocando Protótipo de função -w e -c e de executa.c

--- cli-tools-c/comandos.h
+++ cli-tools-c/comandos.h
@@ -2,7 +2,14 @@
 #ifndef COMANDOS_H
 #define COMANDOS_H
 
-//PROTÓTIPOS DAS FUNÇÕES
+//PROTÓTIPOS DAS FUNÇÕES COMANDO.C
 int contaLinhas(const char *nomeArquivo);
+int contaPalavras(const char *nomeArquivo);
+int contaCaracteres(const char *nomeArquivo);
+
+//PROTÓTIPOS DAS FUNÇÕES EXECUTA_COMANDO.C
+void executaLinhas(const char *arquivo);
+void executaPalavras(const char *arquivo);
+void executaCaracteres(const char *arquivo);
 
 #endif
