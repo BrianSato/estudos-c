38976c5bf93e396c2f9d9618be59f0ecb69853e2
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-05 11:24:57 +0900

    Adiciona nova função unificando comando das contagens

--- cli-tools-c/main.c
+++ cli-tools-c/main.c
@@ -10,18 +10,8 @@ int main(int argc, char *argv[]){
 		printf("Uso: %s <arquivo> \n", argv[0]);
 		return 1;
 	}
-	if(strcmp(argv[2], "-l") == 0){
-	    executaLinhas(argv[1]);
-	}
-	else if(strcmp(argv[2], "-w") == 0){
-	    executaPalavras(argv[1]);
-	}
-	else if(strcmp(argv[2], "-c") == 0){
-	    executaCaracteres(argv[1]);
-	}
-	else{
-	    printf("Comando desconhecido\n");
-	}
+	
+	executaComando(argv[1],argv[2]);
 
     return 0;
 }
