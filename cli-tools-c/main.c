1b8d110489445381368743cc93af1270bfe3eb44
Brian Sato <134103878+BrianSato@users.noreply.github.com>
2026-01-06 16:17:32 +0900

    Implementa funcionalidade HELP

--- cli-tools-c/main.c
+++ cli-tools-c/main.c
@@ -4,13 +4,19 @@
 
 
 int main(int argc, char *argv[]){
+	//CASO O USUÁRIO NÃO COLOQUEI O NÚMERO DE ARGUMENTOS SUFICIENTE
+	if(argc <= 2 &&
+	(strcmp(argv[1], "--help") == 0)){
+		executaHelp();
+		return 0;
+	}
 
 	//VERIFICA SE O ARGUMENTO CONTÉM OS ELEMENTOS NECESSÁRIOS PARA SER EXECUTADO
 	if (argc < 3) {
-		printf("Uso: %s <arquivo> \n", argv[0]);
+		erroUso();
 		return 1;
 	}
-	
+
 	executaComando(argv[1],argv[2]);
 
     return 0;
